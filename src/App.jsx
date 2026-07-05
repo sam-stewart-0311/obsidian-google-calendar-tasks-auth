696191211f89b5c10750c96bf0c0add13e87df2c
Sam Stewart <176049335+sam-stewart-spitfire@users.noreply.github.com>
2026-07-05 14:55:14 +0100

    testing code receiving logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,24 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const params = new URLSearchParams(window.location.search);
+  const code = params.get("code");
+  const error = params.get("error");
+
   return (
-    <>
-    hello
-    </>
+    <div className='container-c main'>
+      <div className='container-c'>
+        {
+          code ? (
+            <span>{code}</span>
+          ) : error ? (
+            <span>{error}</span>
+          ) : 
+          <span>nothing to see here...</span>
+        }
+      </div>
+    </div>
   )
 }
 
